ade6456ba68f2e95ce1450cdc9098c8f62533e7e
ElderVivot <eldervivot@gmail.com>
2019-08-18 22:24:33 -0300

    Implementado a classe Department em model.entities

---
+++ src/aplicattion/Program.java
@@ -0,0 +1,15 @@
+package aplicattion;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Department department1 = new Department(1, "Books");
+		System.out.println(department1);
+
+	}
+
+}
