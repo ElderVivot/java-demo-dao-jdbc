46e2aafc0a0a6bf3b6d77f574900f220b69fae17
ElderVivot <eldervivot@gmail.com>
2019-08-18 22:32:37 -0300

    Implementado classe Seller em model.entities. Alem disso foi testado ela na classe Program

--- src/aplicattion/Program.java
+++ src/aplicattion/Program.java
@@ -1,14 +1,20 @@
 package aplicattion;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Department department1 = new Department(1, "Books");
-		System.out.println(department1);
+		Department department = new Department(1, "Books");
+		System.out.println(department);
+		
+		Seller seller = new Seller(1, "Elder", "elder@gmail.com", new Date(), 3000.0, department);
+		System.out.println(seller);
 
 	}
 
